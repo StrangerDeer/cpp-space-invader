221aa86e72c87db685b7f1e1ba3c2bedeb63c93c
rBabett <richterbabett03@gmail.com>
2024-03-12 13:59:33 +0100

    add random rotation to asteroid

--- objects/Asteroid.h
+++ objects/Asteroid.h
@@ -8,9 +8,10 @@ class Asteroid : public FallingObject, public ScoreObject, public HealthObject
 public:
     Asteroid(int maxHp, int health, int x, int y, int width, int height, int minSpeed, int maxSpeed, bool resettable, int points) :
             HealthObject(health), FallingObject(x, y, width, height, minSpeed, maxSpeed, resettable),
-            ScoreObject(points) {
-        maxHealth = maxHp;
+            ScoreObject(points), maxHealth(maxHp) {
+        getRandomRotation();
     };
+
     bool isDead() {
         if (health <= 0) {
             reset();
@@ -19,11 +20,21 @@ public:
         return false;
     };
 
+    [[nodiscard]] double getRotation() const {return angleRotation;}
+
 private:
     int maxHealth;
+    double angleRotation;
+
+    void getRandomRotation() {
+        int rotation = Util::getRandomNumber(1, 30);
+        angleRotation = (double)rotation / 10;
+    }
+
     void reset() {
         placeAtStartingPos();
         health = maxHealth;
+        getRandomRotation();
     }
 };
 
