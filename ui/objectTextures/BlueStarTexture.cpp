353f5b8ef9d78ae57abac81740eb58effaa977e3
rBabett <richterbabett03@gmail.com>
2024-03-12 14:00:45 +0100

    object texture size fits hitbox size

--- ui/objectTextures/BlueStarTexture.cpp
+++ ui/objectTextures/BlueStarTexture.cpp
@@ -13,6 +13,6 @@ void BlueStarTexture::print(SDL_Renderer *renderer, Uint32 ticks) {
     SDL_QueryTexture(texture, nullptr, nullptr, &bluestar->rect.w, &bluestar->rect.h);
     Uint32 sprite = (ticks / 100) % 6;
     SDL_Rect srcrect = { static_cast<int>(sprite * 195), 0, 195, 500 };
-    SDL_Rect dstrect = { bluestar->rect.x, bluestar->rect.y, 55, 150 };
+    SDL_Rect dstrect = { bluestar->rect.x, bluestar->rect.y, bluestar->width, bluestar->height };
     SDL_RenderCopy(renderer, texture, &srcrect, &dstrect);
 }
--- ui/objectTextures/SpaceshipTexture.cpp
+++ ui/objectTextures/SpaceshipTexture.cpp
@@ -10,6 +10,6 @@ void SpaceshipTexture::print(SDL_Renderer *renderer, Uint32 ticks) {
     SDL_QueryTexture(texture, nullptr, nullptr, &spaceship->rect.w, &spaceship->rect.h);
     Uint32 sprite = (ticks / 100) % 3;
     SDL_Rect srcrect = { static_cast<int>(sprite * 400), 0, 400, 500 };
-    SDL_Rect dstrect = { 10, 10, 400, 500 };
+    SDL_Rect dstrect = { 10, 10, spaceship->width, spaceship->height };
     SDL_RenderCopy(renderer, texture, &srcrect, &dstrect);
 }
