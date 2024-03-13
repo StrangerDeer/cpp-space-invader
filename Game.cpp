a78b40f969c5c8518a3d817a77474820de5a11e0
rBabett <richterbabett03@gmail.com>
2024-03-13 17:42:29 +0100

    adjust number and speed of stars and asteroids

--- Game.cpp
+++ Game.cpp
@@ -129,8 +129,8 @@ void Game::initLogic() {
   constexpr int STAR_HEIGHT = 125;
   constexpr int STAR_WIDTH = 50;
 
-  constexpr int NUMBER_OF_PINK_STARS = 5;
-  constexpr int NUMBER_OF_GREEN_STARS = 4;
+  constexpr int NUMBER_OF_PINK_STARS = 7;
+  constexpr int NUMBER_OF_GREEN_STARS = 5;
   constexpr int NUMBER_OF_BLUE_STARS = 3;
   constexpr int NUMBER_OF_GOLD_STARS = 2;
   constexpr int NUMBER_OF_RED_STARS = 1;
@@ -147,19 +147,19 @@ void Game::initLogic() {
   constexpr int MIN_SPEED_FOR_GOLD_STARS = 10;
   constexpr int MIN_SPEED_FOR_RED_STARS = 15;
 
-  constexpr int MAX_SPEED_FOR_PINK_STARS = 7;
-  constexpr int MAX_SPEED_FOR_GREEN_STARS = 10;
-  constexpr int MAX_SPEED_FOR_BLUE_STARS = 20;
-  constexpr int MAX_SPEED_FOR_GOLD_STARS = 30;
-  constexpr int MAX_SPEED_FOR_RED_STARS = 35;
+  constexpr int MAX_SPEED_FOR_PINK_STARS = 10;
+  constexpr int MAX_SPEED_FOR_GREEN_STARS = 20;
+  constexpr int MAX_SPEED_FOR_BLUE_STARS = 30;
+  constexpr int MAX_SPEED_FOR_GOLD_STARS = 35;
+  constexpr int MAX_SPEED_FOR_RED_STARS = 40;
 
   constexpr int SMALL_ASTEROID_HEIGHT = 70;
   constexpr int MEDIUM_ASTEROID_HEIGHT = 120;
-  constexpr int LARGE_ASTEROID_HEIGHT = 170;
+  constexpr int LARGE_ASTEROID_HEIGHT = 250;
 
-  constexpr int NUMBER_OF_SMALL_ASTEROIDS = 25;
-  constexpr int NUMBER_OF_MEDIUM_ASTEROIDS = 15;
-  constexpr int NUMBER_OF_LARGE_ASTEROIDS = 5;
+  constexpr int NUMBER_OF_SMALL_ASTEROIDS = 20;
+  constexpr int NUMBER_OF_MEDIUM_ASTEROIDS = 10;
+  constexpr int NUMBER_OF_LARGE_ASTEROIDS = 3;
 
   constexpr int HEALTH_OF_SMALL_ASTEROIDS = 1;
   constexpr int HEALTH_OF_MEDIUM_ASTEROIDS = 3;
