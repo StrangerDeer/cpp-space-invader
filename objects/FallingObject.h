de4b72de426c82cf7df7d4cb7d3b70ea3741bb2e
Németh Kristóf <nemethkristof1@gmail.com>
2024-03-13 11:58:25 +0100

    make placeAtStartingPos use window height and width

--- objects/FallingObject.h
+++ objects/FallingObject.h
@@ -2,32 +2,31 @@
 #include "DimensionalObject.h"
 #include "../Util.h"
 
-class FallingObject : public DimensionalObject
-{
-public:
-    FallingObject(int x, int y, int width, int height, int minSpeed, int maxSpeed, bool resettable) :
-            DimensionalObject(x, y, width, height), minSpeed(minSpeed), maxSpeed(maxSpeed), isResettable(resettable) {
-        setRandomSpeed();
-    };
+class FallingObject : public DimensionalObject {
+ public:
+  FallingObject(int x, int y, int width, int height, int minSpeed, int maxSpeed, bool resettable) :
+      DimensionalObject(x, y, width, height), minSpeed(minSpeed), maxSpeed(maxSpeed), isResettable(resettable) {
+    setRandomSpeed();
+  };
 
-    void placeAtStartingPos() {
-        setRandomSpeed();
-        rect.x = 0;
-        rect.y = 0;
-    };
+  void placeAtStartingPos(int windowWidth, int windowsHeight) {
+    setRandomSpeed();
+    rect.x = Util::getRandomNumber(0, windowWidth);
+    rect.y = Util::getRandomNumber(-windowsHeight * 3, 0 - rect.h);
+  };
 
-    void setRandomSpeed() {
-        currentSpeed = Util::getRandomNumber(minSpeed, maxSpeed);
-    }
+  void setRandomSpeed() {
+    currentSpeed = Util::getRandomNumber(minSpeed, maxSpeed);
+  }
 
-    void fall() {
-        rect.y += currentSpeed;
-    }
+  void fall() {
+    rect.y += currentSpeed;
+  }
 
-protected:
-    int minSpeed;
-    int maxSpeed;
-    int currentSpeed{};
-    bool isResettable;
+ protected:
+  int minSpeed;
+  int maxSpeed;
+  int currentSpeed{};
+  bool isResettable;
 };
 
