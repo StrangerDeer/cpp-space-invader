337d7f9b9cdb9c07be2f66926d3606ba00741f12
StrangerDeer <karmanb96@gmail.com>
2024-03-21 15:01:34 +0100

    spaceship stores bullets

--- objects/Spaceship.h
+++ objects/Spaceship.h
@@ -1,7 +1,9 @@
 #pragma once
+#include <vector>
 #include "DimensionalObject.h"
 #include "FlyingObject.h"
 #include "HealthObject.h"
+#include "SpaceshipBullet.h"
 
 class Spaceship : public DimensionalObject, public FlyingObject, public HealthObject
 {
@@ -11,6 +13,8 @@ public:
             lineOfFire(lineOfFire) {
         points = 0;
     };
+
+    std::vector<std::shared_ptr<SpaceshipBullet>> bullets;
 
     void increasePoints(int pts) {
         points += pts;
@@ -23,6 +27,18 @@ public:
     void moveLeft(){
         rect.x -= speed;
     }
+
+    std::shared_ptr<SpaceshipBullet> shoot(int spaceshipWidth, int spaceshipHeight){
+      int x = rect.x + spaceshipWidth / 2;
+      int y = rect.y + spaceshipHeight / 2;
+
+      std::shared_ptr<SpaceshipBullet> bullet = std::make_shared<SpaceshipBullet>(x, y);
+
+      bullets.push_back(bullet);
+
+      return bullet;
+    }
+
     void moveRight(){ rect.x += speed; }
     void moveUp(){ rect.y -= speed;}
     void moveDown(){ rect.y += speed;}
