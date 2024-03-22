88efc7dd2cc265cf0a96c56a92b2bf54fd60f4fd
StrangerDeer <karmanb96@gmail.com>
2024-03-22 11:42:47 +0100

    add GameSoundEffect class

--- sound/GameMusic.cpp
+++ sound/GameMusic.cpp
@@ -2,14 +2,14 @@
 // Created by Deer on 13/03/2024.
 //
 
-#include "BackgroundMusic.h"
+#include "GameMusic.h"
 
-void BackgroundMusic::init() {
+void GameMusic::init(const std::string& filePath) {
     Mix_Init(MIX_INIT_MP3 | MIX_INIT_OPUS | MIX_INIT_OGG);
 
     Mix_OpenAudio(MIX_DEFAULT_FREQUENCY, AUDIO_S16SYS, MIX_DEFAULT_CHANNELS, 4096);
 
-    music = Mix_LoadMUS("../sound/background.wav");
+    music = Mix_LoadMUS(filePath.c_str());
 
     if(!music){
         std::cerr << "Background music error:" << Mix_GetError() << std::endl;
