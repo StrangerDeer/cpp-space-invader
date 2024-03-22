7144ad03ef3a26c2cb436aaded28b4c23f88b33d
StrangerDeer <karmanb96@gmail.com>
2024-03-22 11:43:11 +0100

    change spaceship bullet color

--- ui/objectTextures/BulletTexture.h
+++ ui/objectTextures/BulletTexture.h
@@ -7,7 +7,7 @@ class BulletTexture : public TextureCreator {
 
  public:
   BulletTexture(SDL_Renderer* renderer, const std::shared_ptr<SpaceshipBullet>& bullet) :
-      TextureCreator(renderer, bullet->bulletRect.w, bullet->bulletRect.h, SDL_Color{255,255,255,255}),
+      TextureCreator(renderer, bullet->bulletRect.w, bullet->bulletRect.h, SDL_Color{0,255,0,255}),
       bullet(bullet)
   {}
 
