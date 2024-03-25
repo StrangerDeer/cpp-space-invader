d5563b8559aab6c32029394883855e4cc00fc8ba
rBabett <richterbabett03@gmail.com>
2024-03-25 15:46:15 +0100

    make alien move randomly across the top of the screen

--- objects/Alien.h
+++ objects/Alien.h
@@ -2,6 +2,7 @@
 
 #include <memory>
 #include <vector>
+#include <string>
 #include "DimensionalObject.h"
 #include "HealthObject.h"
 #include "AlienBullet.h"
@@ -11,8 +12,10 @@
 class Alien : public HealthObject, public FallingObject, public ScoreObject {
 public:
     Alien(int maxHp, int x, int y, int width, int height, int points, int windowW, int windowH) :
-            ScoreObject(points), HealthObject(maxHp), FallingObject(x, y, width, height, 10, 20, true),
-            maxHealth(maxHp), windowWidth(windowW), windowHeight(windowH), fallingSpeed(5) {};
+            ScoreObject(points), HealthObject(maxHp), FallingObject(x, y, width, height, 2, 15, true),
+            maxHealth(maxHp), windowWidth(windowW), windowHeight(windowH), fallingSpeed(5) {
+        chooseDestination();
+    };
 
     std::vector<std::shared_ptr<AlienBullet>> bullets;
 
@@ -28,6 +31,8 @@ public:
     void fall() override {
         if (rect.y < windowHeight * 0.035) {
             rect.y += fallingSpeed;
+        } else {
+            move();
         }
     }
 
@@ -48,4 +53,25 @@ protected:
     int maxHealth;
     int fallingSpeed;
     int windowWidth, windowHeight;
+    int chosenDestination;
+
+    void move() {
+        if (rect.x <= chosenDestination + currentSpeed && rect.x >= chosenDestination - currentSpeed) {
+            chooseDestination();
+        }
+        if (rect.x < chosenDestination) {
+            rect.x += currentSpeed;
+        } else if (rect.x > chosenDestination) {
+            rect.x -= currentSpeed;
+        }
+    }
+
+    void chooseDestination() {
+        setRandomSpeed();
+        if (rect.x >= windowWidth / 2) {
+            chosenDestination = Util::getRandomNumber(0, windowWidth / 2);
+        } else {
+            chosenDestination = Util::getRandomNumber(windowWidth / 2, windowWidth - width);
+        }
+    }
 };
