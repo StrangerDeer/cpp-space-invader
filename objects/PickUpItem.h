a101721e25d572048ff38a522167c5bd8e548a48
rBabett <richterbabett03@gmail.com>
2024-04-08 10:12:27 +0200

    create PickUpItem object which HealingItem inherits from

---
+++ objects/PickUpItem.h
@@ -0,0 +1,26 @@
+#pragma once
+
+#include "FallingObject.h"
+
+class PickUpItem : public FallingObject {
+public:
+    PickUpItem(int x, int y, int alienWidth, int alienHeight) :
+            FallingObject(x, y, 70, 130, 5, 5, true), alienWidth(alienWidth), alienHeight(alienHeight) {
+        removeFromScreen();
+    };
+
+    void placeAtSpawnPos(int x, int y) {
+        setRandomSpeed();
+        rect.x = x + alienWidth / 2 - width / 2;
+        rect.y = y + alienHeight / 2 - height / 2;
+    };
+
+    void removeFromScreen() {
+        currentSpeed = 0;
+        rect.x = -width;
+        rect.y = -height;
+    }
+
+protected:
+    int alienWidth, alienHeight;
+};
