05e3c4a3b3918541dc63db2411acd4f8ad7236c2
rBabett <richterbabett03@gmail.com>
2024-05-09 14:02:44 +0200

    add extra parameters to asteroid

--- objects/Asteroid.h
+++ objects/Asteroid.h
@@ -5,11 +5,11 @@
 
 class Asteroid : public FallingObject, public ScoreObject, public HealthObject {
  public:
-  Asteroid(int maxHp, int width, int height, int minSpeed, int maxSpeed, int points) :
+  Asteroid(int maxHp, int width, int height, int minSpeed, int maxSpeed, int points, int minRot, int maxRot, int maxYMultiplier) :
       HealthObject(maxHp), FallingObject(Util::getRandomNumber(0, Config::windowWidth),
-                                         Util::getRandomNumber(-Config::windowHeight * 3, (0 - Config::windowHeight) / 2),
-                                         width, height, minSpeed, maxSpeed, true),
-      ScoreObject(points), maxHealth(maxHp) {
+                                         Util::getRandomNumber(-Config::windowHeight * maxYMultiplier, (0 - Config::windowHeight) / 2),
+                                         width, height, minSpeed, maxSpeed, true, maxYMultiplier),
+      ScoreObject(points), maxHealth(maxHp), minRotation(minRot), maxRotation(maxRot) {
         getRandomRotation();
   };
 
@@ -38,9 +38,10 @@ class Asteroid : public FallingObject, public ScoreObject, public HealthObject {
  private:
   int maxHealth;
   double angleRotation;
+  int minRotation, maxRotation;
 
   void getRandomRotation() {
-    int rotation = Util::getRandomNumber(1, 7);
+    int rotation = Util::getRandomNumber(minRotation, maxRotation);
     angleRotation = (double) rotation / 10;
   }
 };
