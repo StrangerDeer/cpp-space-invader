e3c7fa9c1647b7e3ab9e640ef8ec85a78012658d
rBabett <richterbabett03@gmail.com>
2024-05-22 10:21:26 +0200

    alien inherits from DamageTexture

--- ui/objectTextures/AlienTexture.h
+++ ui/objectTextures/AlienTexture.h
@@ -4,12 +4,13 @@
 #include <iostream>
 #include "../../objects/Alien.h"
 #include "../PNGTexture.h"
+#include "../DamageTexture.h"
 
-class AlienTexture : public PNGTexture {
+class AlienTexture : public DamageTexture{
 
 public:
     AlienTexture(SDL_Renderer* renderer, const std::shared_ptr<Alien>& alien) :
-        PNGTexture(renderer, "../ui/textures/alien.png")
+        DamageTexture(renderer, "../ui/textures/alien.png", "../ui/textures/aliendamage.png")
     {
         if(!alien){
             std::cerr << "Alien doesn't exist!" << std::endl;
