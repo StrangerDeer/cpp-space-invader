652f1dfaca14a205d7618a6645a8421fce272ab9
rBabett <richterbabett03@gmail.com>
2024-06-17 10:00:40 +0200

    remove unnecessary functions from shielditem

--- objects/ShieldItem.h
+++ objects/ShieldItem.h
@@ -7,13 +7,6 @@
 class ShieldItem : public PickUpItem {
     ShieldItem(int x, int y, int spawnObjectWidth, int spawnObjectHeight) :
             PickUpItem(x, y, 70, 130) {
-    }
-
-    void addShield(std::shared_ptr<Spaceship> spaceship) {
-        spaceship->enableShield();
-    }
-
-    void removeShield(std::shared_ptr<Spaceship> spaceship) {
-      spaceship->disableShield();
+      removeFromScreen();
     }
 };
