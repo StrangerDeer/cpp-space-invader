365f8d7eb7c732fb4e42a6ee1358480a767a5ce5
rBabett <richterbabett03@gmail.com>
2024-06-18 10:15:43 +0200

    add explosion class

---
+++ objects/Explosion.h
@@ -0,0 +1,21 @@
+#pragma once
+
+#include "DimensionalObject.h"
+
+class Explosion : public DimensionalObject {
+public:
+    Explosion(int x, int y, int width, int height, Uint32 ticks) :
+      DimensionalObject(x, y, width, height), beginTicks(ticks) {
+    }
+
+    bool checkIfShouldBeDestroyed(Uint32 ticks) const {
+      if (ticks - beginTicks >= 150) {
+        return true;
+      } else {
+        return false;
+      }
+    }
+
+private:
+    Uint32 beginTicks;
+};
