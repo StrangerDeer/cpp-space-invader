7c99d0531b223a0d80b336df78e6dfd3fd47b882
rBabett <richterbabett03@gmail.com>
2024-07-17 10:47:32 +0200

    remove test post request from main

--- main.cpp
+++ main.cpp
@@ -1,22 +1,7 @@
-//#define CPPHTTPLIB_OPENSSL_SUPPORT
 #include "Game.h"
-//#include "httplib.h"
 
 int main(int argc, char *argv[])
 {
-    /*httplib::Client cli("https://127.0.0.1:7059");
-
-    std::string data = "{\n"
-                       "    \"Name\": \"from cpp\",\n"
-                       "    \"Score\": 99,\n"
-                       "    \"TimeSurvived\": 20\n"
-                       "}";
-    auto res = cli.Post(std::string("/api/highscores/add-new"), data, std::string("application/json"));
-
-    std::cout << res << std::endl;*/
-    //std::cout << res->status << std::endl;
-    //std::cout << res->body << std::endl;
-
     Game g;
     g.run();
 
